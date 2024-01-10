a3f5aacb2927a733eeaa17ce9122ada1b01b6be4
Alexander <alex.silcorp@gmail.com>
2024-01-10 13:23:42 +0100

    feat: implement getMaxItems

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -497,8 +497,10 @@ function getHexRGBValues(arr) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  const sortedArr = arr.concat().sort((a, b) => b - a);
+
+  return sortedArr.slice(0, n);
 }
 
 /**
